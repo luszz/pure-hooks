ea9ffc339da660369eb19d3f284902241be85692
luszz11 <luszz11@163.com>
2023-08-28 21:48:38 +0800

    add docs

--- config/config.ts
+++ config/config.ts
@@ -1,4 +1,4 @@
-import { menus } from './menus'
+// import { menus } from './menus'
 
 export default {
     nodeModulesTransform: {
@@ -17,6 +17,37 @@ export default {
           'fusion',
         ],
       ],
+      alias: {
+        pureHooks: process.cwd() + '/packages/hooks/src/index.ts',
+      },
+      resolve: {
+        includes: ['packages/hooks/src', 'docs'],
+      },
+      navs: [
+        {
+          title: "指南",
+          path: "/guide",
+        },
+        {
+          title: "Hooks",
+          path: "/hooks",
+        }
+      ],
+      menus: {
+        '/': [
+          {
+            title: '首页',
+            path: 'index',
+          },
+        ],
+        '/guide': [
+          {
+            title: '介绍',
+            path: '/guide',
+          },
+        ],
+        // '/hooks': menus,
+      },
     mode: "site",
     title: "PureHooks",
     logo: '/logo.png',
